add4d2452a5f0ee0fb51c202d5a38cddf7201cbd
Omomia David <omomiad@gmail.com>
2018-11-29 19:19:12 +0100

    ...

---
+++ test.rb
@@ -0,0 +1,6 @@
+puts "Enter Your First Number"
+first_number = gets.chomp()
+puts "Enter Your Second Number"
+second_number = gets.chomp()
+print "The sum is " 
+puts (first_number.to_f + second_number.to_f)
